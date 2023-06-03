69e2f5b9582a49e475e3d7c3088266775309ee90
LegendH <774659715@qq.com>
2023-06-03 15:34:01 +0800

    'init'

---
+++ components/newsbox/newsbox.vue
@@ -0,0 +1,66 @@
+<template>
+	<view class="newsbox">
+		<view class="pic">
+			<image src="../../static/images/0.jpg" mode="aspectFil"></image>
+		</view>
+		<view class="text">
+			<view class="title">
+				新闻标题恶趣味驱蚊器恶气恶气恶趣味请问请问
+			</view>
+			<view class="info">
+				<text>作者名称</text>
+				<text>998浏览</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "newsbox",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.newsbox {
+		display: flex;
+		justify-content: space-between;
+
+		.pic {
+			width: 230rpx;
+			height: 160rpx;
+
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+
+		.text {
+			flex: 1;
+			padding-left: 20rpx;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			
+			.title {
+				font-size: 36rpx;
+				color: #333;
+				@include ellipsis-line(2);
+			}
+
+			.info {
+				font-size: 26rpx;
+				color: #999;
+				text{
+					padding-right: 20rpx;
+				}
+			}
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="home">
+		<scroll-view scroll-x class="navscroll">
+			<view class="item">类型</view>
+			<view class="item">类型</view>
+			<view class="item">类型</view>
+			<view class="item">类型</view>
+			<view class="item">类型</view>
+			<view class="item">类型</view>
+			<view class="item">类型</view>
+		</scroll-view>
+		<view class="content">
+			<view class="row" v-for="item in 10">
+				<newsbox></newsbox>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.home {
+		.navscroll {
+			height: 100rpx;
+			background-color: #f7f8fa;
+			white-space: nowrap;
+
+			@include noshowscroll;
+
+			.item {
+				font-size: 40rpx;
+				display: inline-block;
+				line-height: 100rpx;
+				padding: 0 30rpx;
+				color: #333;
+			}
+
+		}
+
+		.content {
+			padding: 20rpx;
+
+			.row {
+				border-bottom: 1px dashed #efefef;
+				padding: 15rpx 0;
+			}
+		}
+	}
+</style>
